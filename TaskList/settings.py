192beb5169b96678b39a2eb421b5141b15430ff5
Szymon Sieciński <szymon.siecinski@gmail.com>
2017-05-17 23:24:33 +0200

    poprawka w STATICFILES_STORAGE

--- TaskList/settings.py
+++ TaskList/settings.py
@@ -107,7 +107,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'TaskList', 'static'),
-    os.path.join(BASE_DIR, 'static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
